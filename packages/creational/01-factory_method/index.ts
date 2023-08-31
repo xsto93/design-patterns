f1dba20fb4153c059b3ed664f5363221427ffabe
xsto93 <xsto93@gmail.com>
2023-08-31 20:40:34 +0200

    feat: factort method example

---
+++ packages/creational/01-factory_method/index.ts
@@ -0,0 +1,15 @@
+import { Creator, ProductCreatorA, ProductCreatorB } from "./factory-method";
+
+function clientCode(creator: Creator) {
+    // ...
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+    // ...
+}
+
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ProductCreatorA());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ProductCreatorB());
