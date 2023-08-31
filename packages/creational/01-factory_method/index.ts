f20727a0f4937a3e8946fd1baff8924a15eac312
xsto93 <xsto93@gmail.com>
2023-08-31 20:48:04 +0200

    feat: eslint added

--- packages/creational/01-factory_method/index.ts
+++ packages/creational/01-factory_method/index.ts
@@ -1,15 +1,17 @@
 import { Creator, ProductCreatorA, ProductCreatorB } from "./factory-method";
 
 function clientCode(creator: Creator) {
-    // ...
-    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
-    console.log(creator.someOperation());
-    // ...
+  // ...
+  console.log(
+    "Client: I'm not aware of the creator's class, but it still works."
+  );
+  console.log(creator.someOperation());
+  // ...
 }
 
-console.log('App: Launched with the ConcreteCreator1.');
+console.log("App: Launched with the ConcreteCreator1.");
 clientCode(new ProductCreatorA());
-console.log('');
+console.log("");
 
-console.log('App: Launched with the ConcreteCreator2.');
+console.log("App: Launched with the ConcreteCreator2.");
 clientCode(new ProductCreatorB());
