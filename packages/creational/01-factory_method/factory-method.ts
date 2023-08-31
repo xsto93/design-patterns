07171c19cf5614e047dc747f96937bb785b4ec8d
xsto93 <xsto93@gmail.com>
2023-08-31 20:51:15 +0200

    feat: linter fixed

--- packages/creational/01-factory_method/factory-method.ts
+++ packages/creational/01-factory_method/factory-method.ts
@@ -1,38 +1,38 @@
 interface Product {
-  operation(): string;
+  operation: () => string
 }
 
 export abstract class Creator {
-  public abstract factoryMethod(): Product;
+  public abstract factoryMethod (): Product
 
-  public someOperation(): string {
+  public someOperation (): string {
     // Call the factory method to create a Product object.
-    const product = this.factoryMethod();
+    const product = this.factoryMethod()
     // Now, use the product.
-    return `Creator: The same creator's code has just worked with ${product.operation()}`;
+    return `Creator: The same creator's code has just worked with ${product.operation()}`
   }
 }
 
 export class ProductCreatorA extends Creator {
-  public factoryMethod(): Product {
-    return new Product1();
+  public factoryMethod (): Product {
+    return new Product1()
   }
 }
 
 export class ProductCreatorB extends Creator {
-  public factoryMethod(): Product {
-    return new Product2();
+  public factoryMethod (): Product {
+    return new Product2()
   }
 }
 
 class Product1 implements Product {
-  public operation(): string {
-    return "{Result of the eProduct1}";
+  public operation (): string {
+    return '{Result of the eProduct1}'
   }
 }
 
 class Product2 implements Product {
-  public operation(): string {
-    return "{Result of the Product2}";
+  public operation (): string {
+    return '{Result of the Product2}'
   }
 }
--- packages/creational/01-factory_method/index.ts
+++ packages/creational/01-factory_method/index.ts
@@ -1,17 +1,17 @@
-import { Creator, ProductCreatorA, ProductCreatorB } from "./factory-method";
+import { type Creator, ProductCreatorA, ProductCreatorB } from './factory-method'
 
-function clientCode(creator: Creator) {
+function clientCode (creator: Creator): void {
   // ...
   console.log(
     "Client: I'm not aware of the creator's class, but it still works."
-  );
-  console.log(creator.someOperation());
+  )
+  console.log(creator.someOperation())
   // ...
 }
 
-console.log("App: Launched with the ConcreteCreator1.");
-clientCode(new ProductCreatorA());
-console.log("");
+console.log('App: Launched with the ConcreteCreator1.')
+clientCode(new ProductCreatorA())
+console.log('')
 
-console.log("App: Launched with the ConcreteCreator2.");
-clientCode(new ProductCreatorB());
+console.log('App: Launched with the ConcreteCreator2.')
+clientCode(new ProductCreatorB())
